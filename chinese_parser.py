523deabf6b272657b41362dc191ff2afe49487d9
EC Computer <dontwantno@association.noway>
2024-10-02 09:59:41 -0500

    filter_low_confidence and QOL upgrades to Chinese_parser.py

--- chinese_parser.py
+++ chinese_parser.py
@@ -5,23 +5,56 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
 
-def read_text_from_image(filepath: str, language: str): #open ocr is only for chinese and english
+def read_text_from_image(filepath: str, language: str, **kwargs) -> str: 
+    # returns string of the text detected in the image
+
+    minimum_confidence = kwargs.get('minimum_confidence')
+    print_confidence_levels = kwargs.get('print_confidence_levels')
+    display_text_boxes = kwargs.get('display_text_boxes')
 
     image = Image.open(filepath)
-    text = pytesseract.image_to_string(image, lang=language)  # Use 'chi_sim' for Simplified Chinese
+    d = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT, lang=language)
 
-    print(text)
+    if print_confidence_levels:
+        for i, w in enumerate(d['conf']):
+            print(f"{d['text'][i]} with confidence {d['conf'][i]}")
 
-    img = cv2.imread("E:/SpaCy Playground/uploads/Screenshot.png")
 
-    d = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
-    n_boxes = len(d['level'])
+    text = ''.join(d['text'])
+    if minimum_confidence != None:
+        text = filter_low_confidence(d, minimum_confidence)
+
+    print(text) 
+    
+    if display_text_boxes == True: 
+        display_text_box_image(d, filepath)
+
+       
+    return text
+
+def filter_low_confidence(data: dict, min_confidence: int) -> list:
+    # filters text below a given min_confidence (0-100, 80 being a decent threshold)
+    filtered_data = []
+
+    for i, c in enumerate(data['conf']):
+        if data['conf'][i] > min_confidence:
+            filtered_data.append(data['text'][i])
+
+    filtered_text = ''.join(filtered_data)
+    return filtered_text
+
+def display_text_box_image(data: dict, filepath: str) -> None:
+    # displays text boxes all neat-like
+    img = cv2.imread(filepath)
+    n_boxes = len(data['level'])
     for i in range(n_boxes):
-        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('img', img)
     cv2.waitKey(0)
+    return
+
 
 language = "chi_sim"
-read_text_from_image(filepath="E:/SpaCy Playground/uploads/Screenshot.png", language=language)
+read_text_from_image(filepath="E:/ProjectLexeme_Server/uploads/Screenshot.png", language=language, display_text_boxes=True, minimum_confidence=90, print_confidence_levels=True)
