e43dd60eadcb78621b0c817334017dc0d0b65d4b
EC Computer <dontwantno@association.noway>
2024-10-02 11:51:24 -0500

    working on OCR performance

--- chinese_parser.py
+++ chinese_parser.py
@@ -14,12 +14,11 @@ def read_text_from_image(filepath: str, language: str, preprocessing=False, **kw
     display_text_boxes = kwargs.get('display_text_boxes')
 
     #image = Image.open(filepath)
+    image = cv.imread(filepath)
     if preprocess_image:
-        image = preprocess_image(filepath)
+        image = preprocess_image(image)
     else:
         image = cv.imread(filepath)
-    
-    
 
     d = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT, lang=language)
 
@@ -37,7 +36,7 @@ def read_text_from_image(filepath: str, language: str, preprocessing=False, **kw
     print(text) 
 
     if display_text_boxes == True: 
-        display_text_box_image(d, filepath)
+        display_text_box_image(d, image)
 
        
     return text
@@ -53,10 +52,10 @@ def filter_low_confidence(data: dict, min_confidence: int) -> list:
     filtered_text = ''.join(filtered_data)
     return filtered_text
 
-def display_text_box_image(data: dict, filepath: str) -> None:
+def display_text_box_image(data: dict, img: np.array) -> None:
     # displays text boxes all neat-like
-    img = cv.imread(filepath)
     n_boxes = len(data['level'])
+
     for i in range(n_boxes):
         (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
         cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -65,14 +64,31 @@ def display_text_box_image(data: dict, filepath: str) -> None:
     cv.waitKey(0)
     return
 
-def preprocess_image(filepath: np.array) -> np.array:
+def preprocess_image(img: np.array) -> np.array:
 
-    img = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
-    #
-    return img
+    height, width = img.shape[:2]
+
+    # Calculate the coordinates for the bottom fourth
+    start_row = height // 4 * 3  # Start from 3/4 of the height
+    end_row = height               # End at the bottom of the image
+    start_col = 0                 # Start from the leftmost column
+    end_col = width                # End at the rightmost column
+
+    # Crop the image
+    cropped_image = img[start_row:end_row, start_col:end_col]
+    cropped_image = cv.cvtColor(cropped_image, cv.COLOR_BGR2GRAY)
+
+    # Optionally, display the cropped image
+    cv.imshow('Cropped Image', cropped_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    ret1, th1 = cv.threshold(cropped_image,127,255,cv.THRESH_BINARY)
+    ret1, th1 = cv.threshold(cropped_image,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    preprocessed_image = th1
+    return preprocessed_image
 
 
 
 language = "chi_sim"
 #preprocess_image("E:/ProjectLexeme_Server/uploads/Screenshot.png")
-read_text_from_image(filepath="E:/ProjectLexeme_Server/uploads/Screenshot.png", language=language, preprocessing=True, display_text_boxes=True, minimum_confidence=80, print_confidence_levels=False)
+read_text_from_image(filepath="E:/ProjectLexeme_Server/uploads/Screenshot.png", language=language, preprocessing=True, display_text_boxes=True, minimum_confidence=10, print_confidence_levels=True)
