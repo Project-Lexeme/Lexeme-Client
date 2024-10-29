e95243ca99cc7a54a0b36f43fcc6604ded9d1414
EC Computer <dontwantno@association.noway>
2024-10-29 11:05:35 -0500

    cleaned up and added some TODOs

--- main.py
+++ main.py
@@ -8,29 +8,28 @@ import webbrowser
 from pathlib import Path
 import app
 
-'''
-TODO: check out Koboldcpp for a means to deploy LLM server 
-TODO: review TODOs and fix things
-TODO: look closer at how choices get passed back and forth
-TODO: rework the subtitle.csv naming convention and work in a folder 
+''' 
+TODO: look closer at how choices get passed back and forth 
 TODO: refactor, clean - DO IT. don't be lazy. Big rocks are prompt_generator
 TODO: work on screen recorder OCR performance
 TODO: add support for multiple monitors - Desktopmagic library for python - wil be a headache.
-TODO: TEST packaging the thing up and launching from a clean environment. At home, maybe
-TODO: add support for config file with LLM API info, potentially have user-input required with opening windows if no config file is found?
+TODO: add prompts subdirectory in data/prompts and actually pull from them for all prompting purposes
 
 '''
 
 '''
-Prompts could include the use of previously touched terms as reinforcement - e.g. Give me a summary of x. Please use as many touched terms as possible.
-Pictures
-prompts that broaden context around a target term by teaching related terms
-specify context of terms or choose multiple contexts to define terms
-think about poisoning 
+Pedagogical notes:
+
+-Prompts could include the use of previously touched terms as reinforcement - e.g. Give me a summary of x. Please use as many touched terms as possible.
+-Pictures
+-Prompts that broaden context around a target term by teaching related terms
+-Specify context of terms or choose multiple contexts to define terms
+-Think about poisoning 
 
 '''
 
 '''
+to compile:
 python -m PyInstaller main.spec main.py
 '''
 
